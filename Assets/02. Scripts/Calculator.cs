c9caa3beb4f4224304f510c0bbdffb39badfe7a7
KtadachiWell <kkang001b@gmail.com>
2025-05-20 15:56:53 +0900

    Breaktime
    --

--- Assets/02. Scripts/Calculator.cs
+++ Assets/02. Scripts/Calculator.cs
@@ -4,10 +4,29 @@ public class Calculator : MonoBehaviour
 {
     public int number1;
     public int number2;
+    // public int number1, number2; µµ °¡´É
 
     void Start()
     {
-        
+        int PlusResult = Cal_PlusMethod();
+
+        int MinusResult = Cal_MinusMethod();
+
+        Debug.Log($"µ¡¼À ¿Ï·á, °ªÀº {PlusResult}, »¬¼À ¿Ï·á, °ªÀº {MinusResult}");
+
+    }
+
+    int Cal_PlusMethod()
+    {
+       int result = number1 + number2;
+
+        return result;
+    }
+    int Cal_MinusMethod()
+    {
+        int result = number1 - number2;
+
+        return result;
     }
 
     // Update is called once per frame
