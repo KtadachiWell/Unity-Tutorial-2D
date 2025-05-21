da64274c2c2b120c06b06f38afc2f471e6c993fc
KtadachiWell <kkang001b@gmail.com>
2025-05-21 11:37:40 +0900

    Update StudyArray.cs

--- Assets/02. Scripts/StudyArray.cs
+++ Assets/02. Scripts/StudyArray.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class StudyArray : MonoBehaviour
 {
 
-    int[] arrayNumber = new int[5];
+    int[] arrayNumber = new int[5] {1,2,3,4,5};
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log($"Array의 첫번째 값 : {arrayNumber[0]}");
+        Debug.Log($"Array의 세번째 값 : {arrayNumber[2]}");
+        Debug.Log($"Array의 여섯번째 값 : {arrayNumber[5]}");
     }
 }
