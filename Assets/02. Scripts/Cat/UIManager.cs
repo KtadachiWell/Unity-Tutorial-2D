c12e73f363dab81fd844b89eb9f87cb077a695e1
KtadachiWell <kkang001b@gmail.com>
2025-06-05 10:17:25 +0900

    250605
    --

---
+++ Assets/02. Scripts/Cat/UIManager.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Cat
+{
+    public class UIManager : MonoBehaviour
+    {
+        public InputField inputField;
+        public TextMeshProUGUI nameTextUI;
+    }
+
+}
